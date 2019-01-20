d2fb0b931d67fdc352b47ed904cda3bbe42f7b48
Jeremy Decker <jdecker71@outlook.com>
2019-01-20 16:43:12 -0500

    mappings and put method for admin

--- GoDisneyBlog/Data/GoDisneyMappingProfile.cs
+++ GoDisneyBlog/Data/GoDisneyMappingProfile.cs
@@ -14,10 +14,20 @@ namespace GoDisneyBlog.Data
         public GoDisneyMappingProfile()
         {
             CreateMap<Card, CardViewModel>()
-                .ForMember(c => c.thisCardId, ex => ex.MapFrom(c => c.Id))
+                //.ForMember(c => c.thisCardId, ex => ex.MapFrom(c => c.Id))
+                .ForMember(c => c.cardTitle, ex => ex.MapFrom(c => c.CardTitle))
+                .ForMember(c => c.cardImg, ex => ex.MapFrom(c => c.CardImg))
+                .ForMember(c => c.cardContents, ex => ex.MapFrom(c => c.CardContents))
+                .ForMember(c => c.cardIcon, ex => ex.MapFrom(c => c.CardIcon))
+                .ForMember(c => c.cardLink, ex => ex.MapFrom(c => c.CardLink))
+                .ForMember(c => c.cardLinkName, ex => ex.MapFrom(c => c.CardLinkName))
                 .ReverseMap();
 
             CreateMap<CardContent, CardContentsViewModel>()
+                .ForMember(c => c.paraOne, ex => ex.MapFrom(c => c.ParaOne))
+                .ForMember(c => c.paraTwo, ex => ex.MapFrom(c => c.ParaTwo))
+                .ForMember(c => c.paraThree, ex => ex.MapFrom(c => c.ParaFour))
+                .ForMember(c => c.paraFour, ex => ex.MapFrom(c => c.ParaFour))
                 .ReverseMap();
         }
     }
